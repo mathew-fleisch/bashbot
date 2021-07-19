ad977131923d9d4f453564e0c4637ac4589e0bb8
mathew-fleisch <mathew.fleisch@gmail.com>
2021-07-19 14:05:51 -0700

    Allow configurable base rather than hard-coding api.github.com

--- scripts/get-config.sh
+++ scripts/get-config.sh
@@ -2,10 +2,12 @@
 # shellcheck disable=SC2086
 set -eou pipefail
 
+github_base="${github_base:-api.github.com}"
 expected_variables="GIT_TOKEN github_org github_repo github_branch github_filename"
 for expect in $expected_variables; do
   if [[ -z "${!expect}" ]]; then
     echo "Missing environment variable $expect"
+    echo "Expected: $expected_variables"
     exit 1
   fi
 done
@@ -13,5 +15,5 @@ done
 curl -H "Authorization: token $GIT_TOKEN" \
   -H 'Accept: application/vnd.github.v3.raw' \
   -o ../config.json \
-  -sL https://api.github.com/repos/${github_org}/${github_repo}/contents/${github_filename}?ref=${github_branch} 2>&1
+  -sL https://${github_base}/repos/${github_org}/${github_repo}/contents/${github_filename}?ref=${github_branch} 2>&1
 
