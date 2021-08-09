66d4aa2745b8c60174cbc3a90b94f56944b9c256
mathew-fleisch <mathew.fleisch@gmail.com>
2021-08-08 19:37:28 -0700

    Do that thang

--- examples/github-action/trigger.sh
+++ examples/github-action/trigger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
 github_base="${github_base:-api.github.com}"
 expected_variables="BASHBOT_CONFIG_FILEPATH SLACK_TOKEN SLACK_CHANNEL SLACK_USERID REPO_OWNER REPO_NAME GITHUB_TOKEN"
@@ -6,10 +7,12 @@ for expect in $expected_variables; do
   if [[ -z "${!expect}" ]]; then
     echo "Missing environment variable $expect"
     echo "Expected: $expected_variables"
-    exit 1
+    exit 0
   fi
 done
-headers="-sH \"Accept: application/vnd.github.everest-preview+json\" -H \"Authorization: token ${GITHUB_TOKEN}\" -X POST"
-curl ${headers} \
+curl -s \
+  -X POST \
+  -H "Accept: application/vnd.github.everest-preview+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
   --data '{"event_type":"trigger-github-action","client_payload": {"channel":"'${SLACK_CHANNEL}'", "user_id": "'${SLACK_USERID}'"}}' \
-  https://${github_base}/repos/${REPO_OWNER}/${REPO_NAME}/dispatches 
+  "https://${github_base}/repos/${REPO_OWNER}/${REPO_NAME}/dispatches"
