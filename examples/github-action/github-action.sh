1f918301acd2c09ee034a50406093f87beea1be5
mathew-fleisch <mathew.fleisch@gmail.com>
2021-08-11 16:10:28 -0700

    Temporarily simplify response message for gif

--- examples/github-action/github-action.sh
+++ examples/github-action/github-action.sh
@@ -11,13 +11,11 @@ for expect in $expected_variables; do
 done
 headers="-sH \"Accept: application/vnd.github.everest-preview+json\" -H \"Authorization: token ${GITHUB_TOKEN}\""
 LATEST_VERSION=$(curl ${headers} https://${github_base}/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest | grep tag_name | cut -d '"' -f 4)
-JOB_ID=$(curl ${headers} https://${github_base}/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/${GITHUB_RUN_ID}/jobs | jq -r '.jobs[].id')
 arch=amd64
 [ $(uname -m) == "aarch64" ] && arch=arm64
 os=$(uname | tr '[:upper:]' '[:lower:]')
 wget -q https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/download/${LATEST_VERSION}/bashbot-${os}-${arch} -O bashbot
 chmod +x bashbot
-downloaded_version=$(./bashbot --version | awk '{print $2}')
 ./bashbot \
     --send-message-channel ${SLACK_CHANNEL} \
-    --send-message-text "<@${SLACK_USERID}> Bashbot triggered this <https://github.com/${REPO_OWNER}/${REPO_NAME}/blob/main/.github/workflows/example-bashbot-github-action.yaml|example github action> and used the bashbot binary (<https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/tag/${downloaded_version}|${downloaded_version}>) within the github action, to <https://github.com/${REPO_OWNER}/${REPO_NAME}/runs/${JOB_ID}?check_suite_focus=true|simulate a long running job> in order to send success/failure back to slack."
+    --send-message-text "<@${SLACK_USERID}> Bashbot triggered this job"
