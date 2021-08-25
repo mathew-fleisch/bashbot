efe8c346f769697428fd0d6b7222fb0eb8feaa01
mathew-fleisch <mathew.fleisch@gmail.com>
2021-08-25 04:46:38 -0700

    POC air quality example

---
+++ examples/aqi/aqi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$AIRQUALITY_API_KEY" ]; then
+  echo "Missing Air Now API Key..."
+  echo "[Air Now API](https://docs.airnowapi.org/)"
+  exit 0
+fi
+
+zip=$1
+if [ -z "$zip" ]; then
+  echo "Usage: $0 [zip]"
+  exit 0
+fi
+
+response=$(curl -s "http://www.airnowapi.org/aq/observation/zipCode/current/?zipCode=${zip}&distance=5&format=application/json&API_KEY=${AIRQUALITY_API_KEY}")
+echo "'$response'"
+aqi=$(echo "$response" | jq '.[0]')
+reporting_area=$(echo "$aqi" | jq -r '.ReportingArea')
+aqi_value=$(echo "$aqi" | jq -r '.aqi_value')
+time_stamp="$(echo "$aqi" | jq -r '.DateObserved')$(echo "$aqi" | jq -r '.HourObserved'):00"
+category=$(echo "$aqi" | jq -r '.Category.Name')
+case $category in
+  "Good") emoji=":large_green_circle:";;
+  "Moderate") emoji=":large_yellow_circle:";;
+  "Unhealthy for Sensitive Groups") emoji=":large_orange_circle:";;
+  "Unhealthy") emoji=":red_circle:";;
+  "Very Unhealthy") emoji=":large_purple_circle:";;
+  "Hazardous") emoji=":black_circle:";;
+esac
+if [[ "$aqi_value" != "null" ]]; then
+  echo "Could not find an aqi_value for this zip code";
+else
+  echo "$emoji The <https://docs.airnowapi.org/aq101|Air Quality Index> in $reporting_area is $aqi_value ($category) as of $time_stamp";
+fi
